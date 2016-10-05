ee15c12a19e94fb85189f496f0a4a582bb22dbcf
DivyaV0212 <mailtodivya.vm@gmail.com>
2016-10-05 12:37:05 +0530

    Update 1.js
    Nandha eg

--- 1.js
+++ 1.js
@@ -1,1 +1,4 @@
-alert('Welcome');
+//alert('Welcom
+var statusVal = getClosedLabel();
+assignValue('Cases','51123033fe97b2f8a40f2cdf78655ceb',encodeURIComponent(statusVal),'Status');
+jQuery('status_val').val(statusVal);
