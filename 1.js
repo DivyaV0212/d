c316faf6308154c7acd781a3c618c14da3091f75
DivyaV0212 <mailtodivya.vm@gmail.com>
2016-10-05 15:00:21 +0530

    Update 1.js
    Clse tckt

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
 //alert('Welcome');
 var statusVal = getClosedLabel();
-assignValue('Cases','fe5d01886cc6d861562a63ca8e6ac4a1f5748b4fa7735163"',encodeURIComponent(statusVal),'Status');
+assignValue('Cases','e01c0e3e9779439f0144a79249c30fba1667d9c06fa8aae2"',encodeURIComponent(statusVal),'Status');
 jQuery('status_val').val(statusVal);
